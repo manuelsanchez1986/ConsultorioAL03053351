ea4f635889cffbf73271aab6bd96071d57590a8b
manuelsanchez1986 <al03053351@tecmilenio.mx>
2025-06-06 19:52:00 -0600

    Funcionalidad: Alta de Médico implementada.

--- src/main/java/com/mycompany/consultorioal03053351/Paciente.java
+++ src/main/java/com/mycompany/consultorioal03053351/Paciente.java
@@ -5,33 +5,27 @@
  */
 package com.mycompany.consultorioal03053351;
 
-/**
- *
- * @author spino
- */
 public class Paciente {
     String nombre;
     String apellido;
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getApellido() {
-        return apellido;
-    }
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+
     public String toCSV() {
         return nombre + "," + apellido;
     }
+
     public static Paciente fromCSV(String linea) {
-        String[] partes = linea.split(",");
+        String[] partes = linea.split(",", -1);
         Paciente p = new Paciente();
-        p.setNombre(partes[0]);
-        p.setApellido(partes[1]);
+        p.nombre = partes[0];
+        p.apellido = partes[1];
         return p;
     }
-   }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+}
